336549a6ea8cd707f890b91c046a9b8a97a1196f
Airien <witeengel@hotmail.com>
2016-01-10 16:38:58 +0100

    Packet creation and injection/except protocol

--- PacketAnalayser/MainWindow.xaml.cs
+++ PacketAnalayser/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using SharpPcap.WinPcap;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,7 +29,9 @@ namespace PacketAnalayser
     public partial class MainWindow : MetroWindow
     {
         ICaptureDevice device;
-        String packet="1", prevPacket="1";
+        String packet = "1", prevPacket = "1";
+        
+        WinPcapDevice devo;
 
         bool UDPfilter = false;
         bool ICMPfilter = false;
@@ -38,7 +41,7 @@ namespace PacketAnalayser
         {
             InitializeComponent();
             // Retrieve all capture devices
-                   
+
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -50,7 +53,7 @@ namespace PacketAnalayser
         {
             try
             {
-            
+
                 CaptureDeviceList cdevices = CaptureDeviceList.Instance;
                 if (cdevices.Count >= 1)
                 {
@@ -138,27 +141,29 @@ namespace PacketAnalayser
 
             // Wait for 'Enter' from the user.
             Console.ReadLine();
-           
 
-           
+
+
         }
         private void device_OnPacketArrival(object sender, CaptureEventArgs e)
         {
-            try {
+            try
+            {
                 Packet packet = Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
 
                 if (packet.PayloadPacket != null)
                 {
                     if (UDPfilter)
                     {
-                        if ((UdpPacket)packet.Extract(typeof(UdpPacket))!=null)
+                        if ((UdpPacket)packet.Extract(typeof(UdpPacket)) != null)
                         {
                             this.Dispatcher.Invoke((Action)(() =>
                             {
                                 lbxCapturedPacketList.Items.Add(packet);
                             }));
                         }
-                    }else if (TCPfilter)
+                    }
+                    else if (TCPfilter)
                     {
                         if ((TcpPacket)packet.Extract(typeof(TcpPacket)) != null)
                         {
@@ -193,13 +198,14 @@ namespace PacketAnalayser
                             lbxCapturedPacketList.Items.Add(packet);
                         }));
                     }
-                  
+
                 }
-                
+
 
                 Console.WriteLine(packet);
-               
-            }catch (Exception ex)
+
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine("{0} Exception caught.", ex);
             }
@@ -212,12 +218,13 @@ namespace PacketAnalayser
 
         private void lbxCapturedPacketList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            try { 
-            gbxPacketInfo.Visibility = Visibility.Visible;
-           Packet packet = lbxCapturedPacketList.SelectedItem as Packet;
-            Console.WriteLine(packet.GetType());
-            TcpPacket tcp = (TcpPacket)packet.Extract(typeof(TcpPacket));
-            IpPacket ip = (IpPacket)packet.Extract(typeof(IpPacket));
+            try
+            {
+                gbxPacketInfo.Visibility = Visibility.Visible;
+                Packet packet = lbxCapturedPacketList.SelectedItem as Packet;
+                Console.WriteLine(packet.GetType());
+                TcpPacket tcp = (TcpPacket)packet.Extract(typeof(TcpPacket));
+                IpPacket ip = (IpPacket)packet.Extract(typeof(IpPacket));
                 EthernetPacket ethernet = (EthernetPacket)packet.Extract(typeof(EthernetPacket));
                 UdpPacket udp = (UdpPacket)packet.Extract(typeof(UdpPacket));
                 ICMPv4Packet icmpv4 = (ICMPv4Packet)packet.Extract(typeof(ICMPv4Packet));
@@ -227,84 +234,84 @@ namespace PacketAnalayser
                 PPPPacket pppp = (PPPPacket)packet.Extract(typeof(PPPPacket));
                 LLDPPacket LLDP = (LLDPPacket)packet.Extract(typeof(LLDPPacket));
                 WakeOnLanPacket WOL = (WakeOnLanPacket)packet.Extract(typeof(WakeOnLanPacket));
-            if (tcp != null)
-             {
-                 this.Dispatcher.Invoke((Action)(() =>
-                             {
-                                 tbxInfo.Text = tcp.ToString(StringOutputType.Verbose);
-                                 
-                             }));
-             }
-             if (ip!=null)
-             {
-                 this.Dispatcher.Invoke((Action)(() =>
-                 {
-                     tbxInfo.Text = ip.ToString(StringOutputType.Verbose);
-                 }));
-             }
-             if (ethernet != null)
-             {
-                 this.Dispatcher.Invoke((Action)(() =>
-                 {
-                     tbxInfo.Text = ethernet.ToString(StringOutputType.Verbose);
-                 }));
-             }
-             if (udp != null)
-             {
-                 this.Dispatcher.Invoke((Action)(() =>
-                 {
-                     tbxInfo.Text = udp.ToString(StringOutputType.Verbose);
-                 }));
-             }
-             if (icmpv4 != null)
-             {
-                 this.Dispatcher.Invoke((Action)(() =>
-                 {
-                     tbxInfo.Text = icmpv4.ToString(StringOutputType.Verbose);
-                 }));
-             }
-             if (icmpv6 != null)
-             {
-                 this.Dispatcher.Invoke((Action)(() =>
-                 {
-                     tbxInfo.Text = icmpv6.ToString(StringOutputType.Verbose);
-                 }));
-             }
-             if (igmp != null)
-             {
-                 this.Dispatcher.Invoke((Action)(() =>
-                 {
-                     tbxInfo.Text = igmp.ToString(StringOutputType.Verbose);
-                 }));
-             }
-             if (PPPoE != null)
-             {
-                 this.Dispatcher.Invoke((Action)(() =>
-                 {
-                     tbxInfo.Text = PPPoE.ToString(StringOutputType.Verbose);
-                 }));
-             }
-             if (pppp != null)
-             {
-                 this.Dispatcher.Invoke((Action)(() =>
-                 {
-                     tbxInfo.Text = pppp.ToString(StringOutputType.Verbose);
-                 }));
-             }
-             if (LLDP != null)
-             {
-                 this.Dispatcher.Invoke((Action)(() =>
-                 {
-                     tbxInfo.Text = LLDP.ToString(StringOutputType.Verbose);
-                 }));
-             }
-             if (WOL != null)
-             {
-                 this.Dispatcher.Invoke((Action)(() =>
-                 {
-                     tbxInfo.Text = WOL.ToString(StringOutputType.Verbose);
-                 }));
-             }
+                if (tcp != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                                {
+                                    tbxInfo.Text = tcp.ToString(StringOutputType.Verbose);
+
+                                }));
+                }
+                if (ip != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbxInfo.Text = ip.ToString(StringOutputType.Verbose);
+                    }));
+                }
+                if (ethernet != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbxInfo.Text = ethernet.ToString(StringOutputType.Verbose);
+                    }));
+                }
+                if (udp != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbxInfo.Text = udp.ToString(StringOutputType.Verbose);
+                    }));
+                }
+                if (icmpv4 != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbxInfo.Text = icmpv4.ToString(StringOutputType.Verbose);
+                    }));
+                }
+                if (icmpv6 != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbxInfo.Text = icmpv6.ToString(StringOutputType.Verbose);
+                    }));
+                }
+                if (igmp != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbxInfo.Text = igmp.ToString(StringOutputType.Verbose);
+                    }));
+                }
+                if (PPPoE != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbxInfo.Text = PPPoE.ToString(StringOutputType.Verbose);
+                    }));
+                }
+                if (pppp != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbxInfo.Text = pppp.ToString(StringOutputType.Verbose);
+                    }));
+                }
+                if (LLDP != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbxInfo.Text = LLDP.ToString(StringOutputType.Verbose);
+                    }));
+                }
+                if (WOL != null)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        tbxInfo.Text = WOL.ToString(StringOutputType.Verbose);
+                    }));
+                }
             }
             catch (Exception ex)
             {
@@ -316,7 +323,7 @@ namespace PacketAnalayser
 
         private void btnCFilterProtocol_Click(object sender, RoutedEventArgs e)
         {
-            if(cbxUDP.IsSelected==true)
+            if (cbxUDP.IsSelected == true)
             {
                 UDPfilter = true;
                 ICMPfilter = false;
@@ -355,11 +362,107 @@ namespace PacketAnalayser
 
                 // Close the pcap device
                 device.Close();
-            }           
-             catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine("{0} Exception caught.", ex);
             }
-}
+        }
+        private void btnCapture_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                CaptureDeviceList cdevices = CaptureDeviceList.Instance;
+                if (cdevices.Count >= 1)
+                {
+                    gbxDeviceListI.Header = cdevices.Count + " Devices";
+                    foreach (ICaptureDevice dev in cdevices)
+                    {
+                        if (dev is AirPcapDevice)
+                        {
+                            lbxAirPcapDeviceListI.Items.Add(dev as AirPcapDevice);
+                        }
+                        else if (dev is WinPcapDevice)
+                        {
+                            lbxWinPcapDeviceListI.Items.Add(dev as WinPcapDevice);
+                        }
+                        else if (dev is LibPcapLiveDevice)
+                        {
+                            lbxLibPcapLiveDeviceListI.Items.Add(dev as LibPcapLiveDevice);
+                        }
+                    }
+                }
+                else
+                {
+                    gbxDeviceListI.Header = "No Devices";
+                }
+
+                /* // check device isn't null
+                 if (cdevices.Count < 1 || cdevices == null)
+                     throw new NullReferenceException();*/
+
+                gbxDeviceListI.Visibility = Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} Exception caught.", ex);
+            }
+
+        }
+
+        private void lbxAirPcapDeviceListI_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            gbxDevInfoI.Visibility = Visibility.Visible;
+            AirPcapDevice dev = lbxAirPcapDeviceListI.SelectedItem as AirPcapDevice;
+            gbxDevInfo.DataContext = dev;
+            lbxAdressesI.ItemsSource = dev.Addresses;
+        }
+
+        private void lbxWinPcapDeviceListI_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            gbxDevInfoI.Visibility = Visibility.Visible;
+            devo = lbxWinPcapDeviceListI.SelectedItem as WinPcapDevice;
+            gbxDevInfo.DataContext = devo;
+            lbxAdressesI.ItemsSource = devo.Addresses;
+        }
+
+        private void lbxLibPcapLiveDeviceListI_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            gbxDevInfoI.Visibility = Visibility.Visible;
+            LibPcapLiveDevice dev = lbxLibPcapLiveDeviceListI.SelectedItem as LibPcapLiveDevice;
+            gbxDevInfo.DataContext = dev;
+            lbxAdressesI.ItemsSource = dev.Addresses;
+        }
+
+        private void btnSendPacket_Click(object sender, RoutedEventArgs e)
+        {
+            device = gbxDevInfo.DataContext as ICaptureDevice;
+            // Open the device
+            device.Open();
+
+
+            try
+            {
+                IPAddress ip = IPAddress.Parse(tbxSourceIp.Text);
+                IPAddress ipaddress = System.Net.IPAddress.Parse(tbxDestinationIp.Text);
+                TcpPacket tcpPakje = new TcpPacket(80, 80);
+                IPv4Packet pakje = new IPv4Packet(ip, ipaddress);
+                pakje.PayloadData = System.Text.Encoding.ASCII.GetBytes(tbxPayloadIp.Text);
+                pakje.TimeToLive = int.Parse(tbxTTLIp.Text);
+               // pakje.Protocol = tbxProtocolIp.Text;
+                device.SendPacket(pakje);
+                Console.WriteLine("-- Packet sent successfuly.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("-- " + ex.Message);
+            }
+
+            // Close the pcap device
+            device.Close();
+            Console.WriteLine("-- Device closed.");
+        }
+
     }
 }
